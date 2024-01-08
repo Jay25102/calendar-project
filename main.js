d8b8a4900ac7f3bf892a345011a48a1251c0fa89
jay25102 <jasonhuang25102@gmail.com>
2024-01-08 13:27:18 -0500

    cleaned up code, and added highlighting to current day

--- main.js
+++ main.js
@@ -5,21 +5,18 @@ const WEEK_DAYS = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 // html references
-const generateCalendar = document.querySelector("#generate-calendar");
+// const generateCalendar = document.querySelector("#generate-calendar");
+const weekdayRow = document.querySelector("#weekday-row");
 const calendarTable = document.querySelector("#calendar-table");
 const navTime = document.querySelector("#nav-time");
 
 /**
  * Starts the application by loading all parts of the ui and generating a calendar
- * Todo:
- * import system time
- * set the month and year in nav based on system time
- * determine what day the first of the current month is
- * put date numbers onto the calendar
  * add todo list functionality when hover/click?
  */
 function startApp() {
     setNavTime();
+    createWeekdayHeader();
     createCalendarTable();
 }
 
---
+++ models.js
@@ -0,0 +1,49 @@
+/**
+ * A calendar class that handles the array representation of the calendar so that the dom may be easier to manipulate
+ * 
+ * What do I make an object?
+ */
+class CalendarArr {
+    constructor() {
+        this.calendarTableArr = [];
+        this.firstOfMonth = new Date(2024, 0, 1).getDay();
+        this.lastOfMonth = new Date(2024, 1, 0).getDate();
+        this.todaysDate = new Date().getDate();
+    }
+
+    // fill array with six rows * seven columns for 42 elements
+    generateCells() {
+        for (let i = 0; i < CAL_COLS * CAL_ROWS; i++) {
+            if ((i >= this.firstOfMonth) && (i <= this.lastOfMonth)) {
+                this.calendarTableArr.push(i);
+            }
+            else {
+                this.calendarTableArr.push(-1);
+            }
+        }
+    }
+
+    // creates dom elements based on the array
+    // can probably move this to a new class
+    generateDomCalendar() {
+        for (let i = 0; i < this.calendarTableArr.length; i++) {
+            // every seventh element is the last day of the week
+            // so create a new week using a table row
+            if (i % 7 == 0) {
+                let newTr = document.createElement("tr");
+                newTr.classList.add("calendar-week");
+                calendarTable.appendChild(newTr);
+            }
+            let newTd = document.createElement("td");
+            newTd.classList.add("calendar-day");
+            if (i == this.todaysDate) {
+                newTd.id = "todays-date";
+            }
+            if (this.calendarTableArr[i] != -1) {
+                newTd.innerText = this.calendarTableArr[i];
+            }
+            calendarTable.lastChild.appendChild(newTd);
+        }
+    }
+
+}
